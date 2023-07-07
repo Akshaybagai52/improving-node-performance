20748d9075a45bafc2364549eb5647dd92349490
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-07 13:37:53 +0530

    applied os req

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const cluster = require('cluster');
-const app = express()
+const express = require("express");
+const cluster = require("cluster");
+const app = express();
+const os = require("os");
 
 function delay(duration) {
-    const startTime = Date.now();
-    while ((Date.now()-startTime)< duration){
-    }
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {}
 }
-app.get('/', (req, res) => {
-    res.send(`Performance example: ${process.pid}`);
-})
+app.get("/", (req, res) => {
+  res.send(`Performance example: ${process.pid}`);
+});
 
-app.get('/timer', (req, res) => {
-    delay(9000);
-    res.send(`timer!!!!!!!!!!!!!!!: ${process.pid}`);
-})
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`timer!!!!!!!!!!!!!!!: ${process.pid}`);
+});
 
-if(cluster.isMaster) {
-    console.log("master has been started");
-    cluster.fork()
-    cluster.fork()
+if (cluster.isMaster) {
+  const NUM_FORK = os.cpus().length;
+  for (i = 0; i < NUM_FORK; i++) {
+    cluster.fork();
+  }
+ 
 } else {
-    console.log('Worker process started')
-app.listen(3000, () => console.log("listening on port 3000"))
-
+  console.log("Worker process started");
+  app.listen(3000);
 }
-
-
-
