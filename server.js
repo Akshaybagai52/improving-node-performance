a78a75c2924f3062e12a433cb5dfb9c70355be45
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-07 10:27:19 +0530

    cluster

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const cluster = require('cluster');
 const app = express()
 
+function delay(duration) {
+    const startTime = Date.now();
+    while ((Date.now()-startTime)< duration){
+    }
+}
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.send(`Performance example: ${process.pid}`);
 })
 
+app.get('/timer', (req, res) => {
+    delay(9000);
+    res.send(`timer!!!!!!!!!!!!!!!: ${process.pid}`);
+})
+
+if(cluster.isMaster) {
+    console.log("master has been started");
+    cluster.fork()
+    cluster.fork()
+} else {
+    console.log('Worker process started')
 app.listen(3000, () => console.log("listening on port 3000"))
+
+}
+
+
 
